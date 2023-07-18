9e1d447ed035903c03de2e638572f8ec5909945d
brenor021 <116198391+brenor021@users.noreply.github.com>
2023-07-18 14:53:22 -0300

    cadastro
    kkk

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  appName:'IonicFrontEnd',
+  appSlogan:'Devaneios acumulativos',
+  firebase:{
+  apiKey: "AIzaSyDuXeFD4X7GogXb_KJp7iHP9przLtCwpZ4",
+  authDomain: "meuionicfrontend.firebaseapp.com",
+  projectId: "meuionicfrontend",
+  storageBucket: "meuionicfrontend.appspot.com",
+  messagingSenderId: "1072313783949",
+  appId: "1:1072313783949:web:125dd989e41b304396f4c4"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  appName:'IonicFrontEnd',
+  appSlogan:'Devaneios acumulativos',
+  signInMethod: 'redirect', // redirect || popup
+  policiesDate: '2023-07-07',
+  firebase:{
+  apiKey: "AIzaSyDuXeFD4X7GogXb_KJp7iHP9przLtCwpZ4",
+  authDomain: "meuionicfrontend.firebaseapp.com",
+  projectId: "meuionicfrontend",
+  storageBucket: "meuionicfrontend.appspot.com",
+  messagingSenderId: "1072313783949",
+  appId: "1:1072313783949:web:125dd989e41b304396f4c4"
+  }
+};
+
